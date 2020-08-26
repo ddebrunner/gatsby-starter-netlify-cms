f2e8a08338fb1159dd908331390a201a9f2cac83
Dan Debrunner <dan@stepzen.com>
2020-08-26 08:22:25 -0700

    Load playground at client side

--- src/pages/graphql/playground/index.tsx
+++ src/pages/graphql/playground/index.tsx
@@ -1,15 +1,28 @@
 import * as React from "react";
-
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { Playground, store } from "graphql-playground-react";
+//import { Playground, store } from "graphql-playground-react";
 
 class PlaygroundApp extends React.Component<any> {
   render() {
     return (
-      <Provider store={store}>
-        <Playground endpoint="/.netlify/functions/gql" />
-      </Provider>
+      <div
+        id="container"
+        style={{ width: 800, height: 600, border: "1px solid #ccc" }}
+      ></div>
     );
+  }
+
+  componentDidMount() {
+    import("graphql-playground-react").then((gpr) => {
+      // HERE!!
+      ReactDOM.render(
+        <Provider store={gpr.store}>
+          <gpr.Playground endpoint="/.netlify/functions/gql" />
+        </Provider>,
+        document.body
+      );
+    });
   }
 }
 
